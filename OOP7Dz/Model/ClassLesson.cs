1815daf5bdc19ca9e3b00bb6dd7cb158d7e7ade8
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-09-17 17:50:11 +0300

    Класс  где есть: учитель, урок, и список студентов

---
+++ OOP7Dz/Model/ClassLesson.cs
@@ -0,0 +1,33 @@
+﻿using OOP7Dz.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7Dz.Model
+{
+    internal class ClassLesson
+    {
+        private Teacher teacher;
+        private Lesson lesson;
+        private List<Student> students;
+
+        public ClassLesson(Teacher teacher, Lesson lesson, List<Student> students)
+        {
+            this.teacher = teacher;
+            this.lesson = lesson;
+            this.students = students;
+        }
+
+        public Teacher Teacher { get => teacher; set => teacher = value; }
+        public Lesson Lesson { get => lesson; set => lesson = value; }
+        public List<Student> Students { get => students; set => students = value; }
+
+        public void UpdateClassLessonTeacher(Teacher teacher)
+        {
+            this.teacher = teacher;
+        }
+
+    }
+}
