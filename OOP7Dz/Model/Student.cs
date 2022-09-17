f881cbe4cced46383ad97a9f93b69f7da7cbd586
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-09-17 17:47:04 +0300

    Метод Студент наследуемый от абстрактного класса Person и интерфейса skill

---
+++ OOP7Dz/Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using OOP7Dz.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7Dz.Model
+{
+    public class Student : Person,Skill
+    {
+        public Student(string name, int age) : base(name, age)
+        {
+        }
+
+        public void printSkill()
+        {
+            Console.WriteLine("Меня зовут " + base.Name + " Навыков нет, я только учусь");
+        }
+    }
+   
+}
